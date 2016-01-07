7457afbe67cb19c9834934a84797787986f855dd
jagrutipatil <jagrutipatil32@gmail.com>
2016-01-06 19:03:16 -0800

    Pascal's Triangle II

---
+++ Pascal's_Triangle_II.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+ 		Integer[] row = new Integer[rowIndex+1];
+ 		row[0] = 1;
+ 		for (int i = 1; i <= rowIndex; i++) {
+ 		    row[i] = 1;
+ 		    for (int j = i-1; j > 0; j--) {
+ 			    row[j] = row[j] + row[j-1];
+ 		    }      
+ 		}    
+ 		return new ArrayList<Integer>(Arrays.asList(row));
+    }
+}
