59e3fd9999417b8eecfd74154912e507300c047a
jagrutipatil <jagrutipatil32@gmail.com>
2016-01-18 23:25:16 -0800

    Valid Sudoku

---
+++ Valid_Sudoku.java
@@ -0,0 +1,44 @@
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+ 		Set<Character> set1 = new HashSet<Character>();
+ 		Set<Character> set2 = new HashSet<Character>();
+ 		Set<Character> set3 = new HashSet<Character>();
+
+ 		for (int i = 0; i < 9; i++) {
+ 			set1.clear();
+ 			set2.clear();
+ 			set3.clear();
+ 			for (int j = 0; j < 9; j++) {
+ 				Character ch = board[i][j];
+ 				if (Character.isDigit(ch)) {
+ 					if (!set1.contains(ch)) {
+ 						set1.add(ch);
+ 					} else {
+ 						return false;
+ 					}
+ 				}
+
+ 				ch = board[j][i];
+ 				if (Character.isDigit(ch)) {
+ 					if (!set2.contains(ch)) {
+ 						set2.add(ch);
+ 					} else {
+ 						return false;
+ 					}
+ 				}
+
+ 				int x = i/3*3+ j/3;
+ 				int y = i%3*3 + j%3;
+ 				ch = board[x][y];
+ 				if (Character.isDigit(ch)) {
+ 					if (!set3.contains(ch)) {
+ 						set3.add(ch);
+ 					} else {
+ 						return false;
+ 					}
+ 				}
+ 			}
+ 		}
+ 		return true;
+    }
+}
