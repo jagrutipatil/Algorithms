6cf2bd8b853f5c9d24a703480648e1715a9f0d3e
jagrutipatil <jagrutipatil32@gmail.com>
2016-07-06 21:34:29 -0700

    Peak Element in O(logn)

--- Find Peak Element.java
+++ Find Peak Element.java
@@ -1,25 +1,53 @@
-public class Solution {
-    public int findPeakElement(int[] nums) {
- 		int curr = 0 , prev = 0, next = 0, max = 0, index = 0;
-        
-        if (nums.length > 0)
-            max = nums[0];
-            
- 		for (int i = 1; i <= nums.length-2; i++) {
- 			curr = nums[i];
- 			prev = nums[i-1];
- 			next = nums[i+1];
- 			
- 			if (curr > prev && curr > next && curr > max) {
- 			    index = i;
- 			    max = curr;
- 			}
- 		}   
- 		
- 		if (nums[nums.length-1] > max) {
- 		    index = nums.length -1;
- 		}
 
- 		return index;    
+/**
+* Complexity O(n)
+*/
+class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+    	int curr = 0, prev = 0, next = 0;
+    	for (int i = 1; i < A.length -1 ; i++) {
+    		curr = A[i];
+    		prev = A[i-1];
+    		next = A[i+1];
+
+    		if (prev < curr && curr > next) {
+    			reuturn i;
+    		}
+    	}
+    	return -1;
+    }
+}
+
+//____________________________________________
+
+/**
+* Complexity O(log n)
+* Assumption: at least one peak element exists
+*/
+
+class Solution {
+    /**
+     * @param A: An integers array.
+     * @return: return any of peek positions.
+     */
+    public int findPeak(int[] A) {
+        int start = 0, end = A.length - 1, mid = 0;
+        
+        while (start +1 < end) {
+            mid = (start+end)/2;
+            if (A[mid] < A[mid-1]) {
+                end = mid;
+            } else if (A[mid] < A[mid+1]) {
+                start = mid;
+            } else {
+                return mid;
+            }
+        }
+        
+        return -1;
     }
 }
