c2fcd0ba452da2bff09f562c27976085afc78645
jagrutipatil <jagrutipatil32@gmail.com>
2016-07-08 23:52:29 -0700

    Cosmatic changes

--- Trailing Zeros.java
+++ Trailing Zeros.java
@@ -1,3 +1,14 @@
+/**
+*
+*Write an algorithm which computes the number of trailing zeros in n factorial.*
+*
+*Example
+*11! = 39916800, so the out should be 2
+*
+*Logic: count number of 5 in factorial, number of 2*5 will give number of trailing zeros generated
+*ssNote: there are equal or number of 2 in factorial of any number, and 25 --> 5*5, so need to take into account extra 5 generated also
+*/
+
 class Solution {
     /*
      * param n: As desciption
