472b8ef5447020919a35db53cfc3de6af6855113
jagrutipatil <jagrutipatil32@gmail.com>
2016-07-31 18:47:45 -0700

    Added Problem Statement

--- Word Ladder II.java
+++ Word Ladder II.java
@@ -1,3 +1,22 @@
+/*
+Word Ladder II
+
+Given two words (start and end), and a dictionary, find all shortest transformation sequence(s) from start to end, such that:
+
+Only one letter can be changed at a time
+Each intermediate word must exist in the dictionary
+
+Given:
+start = "hit"
+end = "cog"
+dict = ["hot","dot","dog","lot","log"]
+Return
+  [
+    ["hit","hot","dot","dog","cog"],
+    ["hit","hot","lot","log","cog"]
+  ]
+*/
+
 class WordInfo {
 	String word;
 	int step;
