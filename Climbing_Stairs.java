a84d1221e8af30179158a9cd0d9a7fb4eeae1ca9
jagrutipatil <jagrutipatil32@gmail.com>
2016-08-05 00:30:53 -0700

    Climbing Stairs

--- Climbing_Stairs.java
+++ Climbing_Stairs.java
@@ -1,3 +1,19 @@
+/*
+Climbing Stairs
+
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example
+Given an example n=3 , 1+1+1=2+1=1+2=3
+
+return 3
+
+Logic: This is Dynamic programming problem. for any step n, all unique combination to reach n are unique_combination(n-1) + unique_combination(n-2)
+	         
+*/
+
 public class Solution {
     public int climbStairs(int n) {
  		if (n == 0)
