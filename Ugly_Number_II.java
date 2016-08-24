2ff81c7fd4e5c4c17fa2044bf5c465410172ade6
jagrutipatil <jagrutipatil32@gmail.com>
2016-08-24 12:49:55 -0700

    Added description

--- Ugly_Number_II.java
+++ Ugly_Number_II.java
@@ -1,3 +1,16 @@
+/*
+Ugly number is a number that only have factors 2, 3 and 5.
+
+Design an algorithm to find the nth ugly number. The first 10 ugly numbers are 1, 2, 3, 4, 5, 6, 8, 9, 10, 12...
+
+e.g: n=9, return 10.
+
+Logic: 1. Just multiply 2,3,5 with previous ugly numbers and pick minimum as next ugly number. Keep track of 2,3,5's multiplier with i,j,k pointers to alreaddy calculated ugly numbers
+	   2. multyplying previously calculted ugly number will result in ugly number only. 
+	   3. Note we need only numbers those have factors 2,3,5.
+	   	  e.g: 14 is not ugly number because its factors are 2*7. So to find minimum each time we can not multiply 1,2,...n incrementing sequentially to 2,3,5 bcz it will add unwanted extra factors
+
+*/
 public class Solution {
     public int nthUglyNumber(int n) {
  		int[] dp = new int[n];
