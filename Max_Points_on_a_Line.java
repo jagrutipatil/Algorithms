a527e27a6f078a1753eb156a34c6df205476adb6
jagrutipatil <jagrutipatil32@gmail.com>
2016-08-30 20:19:03 -0700

    Max Points on a Line: Added description and logic

--- Max_Points_on_a_Line.java
+++ Max_Points_on_a_Line.java
@@ -1,3 +1,19 @@
+/*
+Max Points on a Line
+
+Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+
+Example
+Given 4 points: (1,2), (3,6), (0,0), (1,3).
+
+The maximum number is 3.
+
+Complexity: O(n2)
+
+Logic: 1. Two points are on same line if their slope is same, so maintain slope vs count,
+       2. Also count if there are any identical points
+       3. Keep track of maximum points on same line while calculating slope of each point with another point
+*/
 /**
  * Definition for a point.
  * class Point {
