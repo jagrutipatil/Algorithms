0d0331a4cdd5c5e7766a3b605a5436f0e7c21a86
jagrutipatil <jagrutipatil32@gmail.com>
2016-09-07 14:19:55 -0700

    Longest Substring without repeating Characters: Added efficient approach

--- Longest_Substring_Without_Repeating_Characters.java
+++ Longest_Substring_Without_Repeating_Characters.java
@@ -79,3 +79,60 @@ public class Solution {
         }
     }
 }
+
+
+
+____________________________________________________________________________________________
+
+//Another efficient solution
+/* Template for all substring problems
+
+   int findSubstring(string s){
+        vector<int> map(128,0);
+        int counter; // check whether the substring is valid
+        int begin=0, end=0; //two pointers, one point to tail and one  head
+        int d; //the length of substring
+
+        for() {  initialize the hash map here  }
+
+        while(end<s.size()){
+
+            if(map[s[end++]]-- ?){   modify counter here }
+
+            while( counter condition){ 
+                 
+                  update d here if finding minimum
+
+                //increase begin to make it invalid/valid again
+                
+                if(map[s[begin++]]++ ?){ modify counter here }
+            }  
+
+            update d here if finding maximum
+        }
+        return d;
+  }        
+
+One thing needs to be mentioned is that when asked to find maximum substring, we should update maximum after the inner while loop to guarantee that the substring is valid. On the other hand, when asked to find minimum substring, we should update minimum inside the inner while loop.  
+*/
+
+
+public class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int begin = 0, end = 0;
+        int count = 0, len = 0;
+        int[] map = new int[256];
+
+        while (end < s.length()) {
+            if (map[s.charAt(end++)]++ == 1)
+                count++;
+            while (count != 0) {
+                if (map[s.charAt(begin++)]-- == 2)
+                    count--;
+            }
+            len = Math.max(len, end - begin);
+        }
+
+        return Math.max(len, end - begin);
+    }
+}
