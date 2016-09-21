f8ae70b3ce3946d56fb8b457de1ef34f7a95e162
jagrutipatil <jagrutipatil32@gmail.com>
2016-09-21 00:19:12 -0700

    Super Ugly Number: Added description

--- Super Ugly Number.java
+++ Super Ugly Number.java
@@ -1,3 +1,10 @@
+/*
+	Super Ugly Number
+	
+	Write a program to find the nth super ugly number.
+
+	Super ugly numbers are positive numbers whose all prime factors are in the given prime list primes of size k. For example, [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
+*/
 public class Solution {
     public int nthSuperUglyNumber(int n, int[] primes) {
  		int[] result = new int[n];
