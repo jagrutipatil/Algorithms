4c55e6d2b8951717faa763d23e5a981e85f6679e
jagrutipatil <jagrutipatil32@gmail.com>
2016-09-21 15:15:12 -0700

    Basic Calculator : Added description

--- Basic_Calculator.java
+++ Basic_Calculator.java
@@ -1,3 +1,18 @@
+/*
+	Basic Calculator
+
+	Implement a basic calculator to evaluate a simple expression string.
+
+	The expression string may contain open ( and closing parentheses ), the plus + or minus sign -, non-negative integers and empty spaces .
+
+	You may assume that the given expression is always valid.
+
+	Some examples:
+		"1 + 1" = 2
+		" 2-1 + 2 " = 3
+		"(1+(4+5+2)-3)+(6+8)" = 23
+*/
+
 public class Solution {
     public int calculate(String s) {
  		int result = 0, sign = 1, number = 0;
