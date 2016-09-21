21dbc76818d2f1d49ef5fbe8d940b6d36ba14b56
jagrutipatil <jagrutipatil32@gmail.com>
2016-09-20 20:26:27 -0700

    Perfect Squares : Added description

--- Perfect_Squares.java
+++ Perfect_Squares.java
@@ -1,3 +1,13 @@
+/*
+	Perfect Squares
+
+	Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+
+	Example
+		Given n = 12, return 3 because 12 = 4 + 4 + 4
+		Given n = 13, return 2 because 13 = 4 + 9
+*/
+
 public class Solution {
     public int numSquares(int n) {
  		int[] dp = new int[n+1];
