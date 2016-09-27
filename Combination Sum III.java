0adaf3efcdbf4eac04beff4a2cedd5c5d8682668
jagrutipatil <jagrutipatil32@gmail.com>
2016-09-26 23:41:13 -0700

    Combination Sum III: Added alternate implmentation

--- Combination Sum III.java
+++ Combination Sum III.java
@@ -1,3 +1,26 @@
+/*
+    Combination Sum III
+    
+    Find all possible combinations of k numbers that add up to a number n, given that only numbers from 1 to 9 can be used and each combination should be a unique set of numbers.
+
+
+    Example 1:
+
+    Input: k = 3, n = 7
+
+    Output:
+
+    [[1,2,4]]
+    
+    Example 2:
+
+    Input: k = 3, n = 9
+
+    Output:
+
+    [[1,2,6], [1,3,5], [2,3,4]]
+
+*/
 public class Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
  		List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -17,5 +40,32 @@ public class Solution {
     		combination(result, temp, k, i+1, n-i);
     		temp.remove(temp.size()-1);
     	}
+    }
+}
+
+
+___________________________________________
+//Small optimation if partial sum is less than target then and then only iterate through remaining integers
+
+public class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        combinationSum3(k, n, 0, 1, new ArrayList<Integer>(), result);
+        return result;
+    }
+
+    private void combinationSum3(int k, int n, int sum, int start, List<Integer> list, List<List<Integer>> result) {
+        if (list.size() == k) {
+            if (sum == n) {
+                result.add(new ArrayList<Integer>(list));
+            }
+            return;
+        }
+
+        for (int i = start; i <= 9 && sum + i <= n; i++) {
+            list.add(i);
+            combinationSum3(k, n, sum+i, i+1, list, result);
+            list.remove(list.size()-1);
+        }
     }
 }
