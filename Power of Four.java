2a5d1d83f0cdc538d6f579731c5453174c9b1d4f
Jagruti Patil <jagrutipatil@users.noreply.github.com>
2017-07-28 21:21:04 -0700

    Update Power of Four.java

--- Power of Four.java
+++ Power of Four.java
@@ -26,3 +26,26 @@ public class Solution {
  		return count1 == 1 && (count0%2== 0);
     }
 }
+
+// Another way
+/*
+   1 = 1
+   4 = 100
+   8 = 1000
+   16 = 10000
+   
+   logic keep shifting 1 by 2 positions in left and check if its power of four        
+*/
+
+  public class Solution {
+    public boolean isPowerOfFour(int num) {
+        if (num <= 0)
+            return false;
+        int no = 1;
+        for (int i = 0; i < 32; i+=2) {
+            if (num == (no << i))
+                return true;                
+        }
+        return false;            
+    }
+  }
