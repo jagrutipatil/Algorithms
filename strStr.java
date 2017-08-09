731e4b24ca9eac075c69d59ac5a688861a03af6b
Jagruti Patil <jagrutipatil@users.noreply.github.com>
2017-08-08 23:34:58 -0700

    Update strStr.java

--- strStr.java
+++ strStr.java
@@ -39,3 +39,23 @@ class Solution {
         return -1;
     }
 }
+
+
+//Clean Version
+
+public class Solution {
+    public int strStr(String haystack, String needle) {                
+    	for (int i = 0; ; i++) {
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) 
+                    return i;
+                
+                if (i+j == haystack.length()) 
+                    return -1;
+                
+                if (haystack.charAt(i+j) != needle.charAt(j))
+                    break;
+            }
+        }
+    }
+}
